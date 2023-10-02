c412df42fb2caab4154c01593473c0f835095d99
Buyaki01 <rittahbuyaki@gmail.com>
2023-10-02 13:06:16 +0300

    Add components/Provider.js and app/api/auth/[...nextauth]/route.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+    <div className="flex items-center w-screen h-screen">
+      <div className="text-center w-full">
+        <button className=" text-white bg-slate-700 p-2 rounded-lg">Login with Google</button>
+      </div>
+    </div>
   )
 }
