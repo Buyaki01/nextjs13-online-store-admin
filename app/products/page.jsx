c2a30397e621a8f311930c0838e4063399db3df0
Buyaki01 <rittahbuyaki@gmail.com>
2023-10-03 10:23:22 +0300

    Add side navbar to the Products component

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,5 +1,26 @@
+'use client'
+
+import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from "../components/Nav"
+
 export default function Products() {
+
+  const { data: session } = useSession()
+
+  if (!session) {
+    return (
+      <div className="flex items-center w-screen h-screen">
+        <div className="text-center w-full">
+          <button onClick={() => signIn('google')} className=" text-white bg-slate-700 p-2 rounded-lg">Login with Google</button>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div>Products</div>
+    <div className="admin-panel-container min-h-screen flex">
+      <Nav />
+      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">Products</div>
+    </div>
   )
 }
