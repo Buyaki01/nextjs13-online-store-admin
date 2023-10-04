d127cfebfc7317d1b03ef4eccdb0c8bc99d7d8fc
Buyaki01 <rittahbuyaki@gmail.com>
2023-10-04 13:30:17 +0300

    Add component for Edit and add useEffect to fetch the product that matches with the ID on the params

--- app/products/edit/[id]/page.jsx
+++ app/products/edit/[id]/page.jsx
@@ -1,4 +1,30 @@
+'use client'
+
+import { useParams } from "next/navigation"
+import { useEffect } from "react"
+
 export default function EditProduct() {
+
+  const params = useParams()
+
+  const { id } = params
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        if (id) {
+          const response = await axios.put(`/api/products/${id}`)
+          console.log(response.data)
+        }
+      } catch (error) {
+        console.error("Error fetching product:", error)
+      }
+    }
+
+    fetchProduct()
+  
+  }, [id])
+
   return (
     <div>Edit</div>
   )
