c4a3dce5d147be831d6836801ed88cf19233c9d0
Buyaki01 <rittahbuyaki@gmail.com>
2023-10-04 10:56:33 +0300

    Create a layout for products

---
+++ app/products/layout.jsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from "../components/Nav"
+
+export default function ProductsLayout({ children }) {
+
+  const { data: session } = useSession()
+
+  if (!session) {
+    return (
+      <div className="flex items-center w-screen h-screen">
+        <div className="text-center w-full">
+          <button onClick={() => signIn('google')} className=" text-white bg-slate-700 p-2 rounded-lg">Login with Google</button>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div className="admin-panel-container min-h-screen flex">
+        <Nav />
+        <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
+          {children}
+        </div>
+      </div>
+    </>
+  )
+}
