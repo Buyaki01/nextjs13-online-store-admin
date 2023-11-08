11fa0c3f355ed5bd0439f661d8c498722a8c5a46
Buyaki01 <rittahbuyaki@gmail.com>
2023-11-08 14:20:28 +0300

    Update Order model

--- models/order.js
+++ models/order.js
@@ -1,23 +1,27 @@
 const { Schema, model, models, default: mongoose } = require("mongoose")
 
 const OrderSchema = new Schema({
-  // user: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: "User",
-  //   required: true
-  // },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
   // customerId: { type: String }, //It's from Stripe and is different from the user Id
   paymentIntentId: { type: String },
   products: [{
     id: { type: String },
     productName: { type: String },
+    description: { type: String },
     price: { type: String },
-    image: { type: String },
+    images: [{ type: String }],
+    selectedCategory: { type: mongoose.Types.ObjectId },
+    properties: {type:Object},
+    isFeatured: { type: Boolean },
     cartQuantity: { type: Number },
   }],
-  totalPrice: { type: Number, required: true},
+  //totalPrice: { type: Number, required: true},
   deliveryStatus: { type: String, default: "Pending" },
-  paymentStatus: { type: String, required: true },
+  paymentStatus: { type: String, default: "Pending" },
   city: { type: String, required: true },
   postalCode: { type: String, required: true },
   streetAddress: { type: String, required: true },
