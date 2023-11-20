72d878080d1ee4cfbafa6c7118954dc3b0995f4d
Buyaki01 <rittahbuyaki@gmail.com>
2023-11-20 07:37:17 +0300

    Style the Edit and delete buttons, update auth/[...nextauth]/route path

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -7,28 +7,40 @@ export const authOptions = {
   providers: [
     GoogleProvider({
       profile(profile) {
-        //console.log(profile)
-        return { 
-          ...profile,
-          role: profile.role ?? "user" 
+        console.log("This is the profile output: ", profile)
+
+        let userRole = "user"
+        if (profile && profile.email ==="rittahbuyaki@gmail.com") {
+          userRole = "admin"
         }
+
+        const updatedProfile = {
+          ...profile,
+          id: profile?.sub,
+          role: userRole,
+        };
+      
+        console.log("Updated profile output: ", updatedProfile);
+      
+        return updatedProfile;
       },
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
   ],
+  // callbacks: {
+  //   async jwt({ token, user }) {
+  //     if (user && token) {
+  //       token.role = user.role
+  //     }
+  //     return token
+  //   },
+  //   async session({ session, token }) {
+  //     if (session?.user) session.user.role = token.role
+  //     return session
+  //   }
+  // },
   adapter: MongoDBAdapter(clientPromise),
-
-  callbacks: {
-    jwt({ token, user }) {
-      if(user) token.role = user.role
-      return token
-    },
-    session({ session, token }) {
-      session.user.role = token.role
-      return session
-    }
-  }
 }
 
 const handler = NextAuth(authOptions)
