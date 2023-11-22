ceef993f8ebfc87728daf9022aa1c1fd3c85326f
Buyaki01 <rittahbuyaki@gmail.com>
2023-11-22 05:39:55 +0300

    Update authOptions

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,43 +4,37 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from '../../../../lib/mongodb'
 
 export const authOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
       profile(profile) {
-        console.log("This is the profile output: ", profile)
-
         let userRole = "user"
         if (profile && profile.email ==="rittahbuyaki@gmail.com") {
           userRole = "admin"
         }
 
-        const updatedProfile = {
+        return {
           ...profile,
           id: profile?.sub,
           role: userRole,
-        };
-      
-        console.log("Updated profile output: ", updatedProfile);
-      
-        return updatedProfile;
+        }
       },
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
   ],
-  // callbacks: {
-  //   async jwt({ token, user }) {
-  //     if (user && token) {
-  //       token.role = user.role
-  //     }
-  //     return token
-  //   },
-  //   async session({ session, token }) {
-  //     if (session?.user) session.user.role = token.role
-  //     return session
-  //   }
-  // },
-  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user }
+    },
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role
+      return session
+    }
+  },
 }
 
 const handler = NextAuth(authOptions)
