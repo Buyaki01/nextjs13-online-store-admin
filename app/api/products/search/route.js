48d8c69a3e13f2b0dc2e0f07c4e6759e8492b786
Buyaki01 <rittahbuyaki@gmail.com>
2023-12-04 13:28:34 +0300

    Update Product model

--- app/api/products/search/route.js
+++ app/api/products/search/route.js
@@ -5,7 +5,7 @@ import Product from "../../../../models/product"
 export async function GET(request) {
   await connectMongoDB()
 
-  const products = await Product.find().populate('selectedCategory')
+  const products = await Product.find().populate('selectedCategory').populate('brand')
   const { searchParams } = new URL(request.url)
   const query = searchParams.get('query')
   const filteredSearchProducts = products.filter((product) => {
