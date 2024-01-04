ce05f577ba7b0a1c71433f18a656676359266979
Buyaki01 <rittahbuyaki@gmail.com>
2024-01-04 13:35:18 +0300

    Update routes to GET brands, products and for search products

--- app/api/products/search/route.js
+++ app/api/products/search/route.js
@@ -1,9 +1,14 @@
 import { NextResponse } from "next/server"
 import connectMongoDB from '../../../../lib/mongoose'
 import Product from "../../../../models/product"
+import Category from "../../../../models/category"
+import Brand from "../../../../models/brand"
 
 export async function GET(request) {
   await connectMongoDB()
+
+  await Category.find()
+  await Brand.find().populate('parentCategory')
 
   const products = await Product.find().populate('selectedCategory').populate('brand')
   const { searchParams } = new URL(request.url)
